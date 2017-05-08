607c5f531d0be1f9c3cea7d02506508da249bc6f
E. Dunham <github@edunham.net>
2017-05-08 11:42:37 -0700

    bump minor version with new config addition

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='homu',
-    version='0.2.0',
+    version='0.3.0',
     author='Barosl Lee',
     url='https://github.com/barosl/homu',
     description=('A bot that integrates with GitHub '
