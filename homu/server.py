rust-lang/homu

commit 963762a14df0dbe77a82e1b2fb75fd952b3e0b9b

add a 404 page

homu/server.py
@@ -28,6 +28,7 @@ from bottle import (
     redirect,
     abort,
     response,
+    error,
 )
 from threading import Thread
 import sys
@@ -116,6 +117,9 @@ def result(repo_label, pull):
 
 @get('/queue/<repo_label:path>')
 def queue(repo_label):
+    if repo_label not in g.cfg['repo']:
+        abort(404)
+
     logger = g.logger.getChild('queue')
 
     lazy_debug(logger, lambda: 'repo_label: {}'.format(repo_label))
@@ -197,6 +201,9 @@ def queue(repo_label):
 
 @get('/retry_log/<repo_label:path>')
 def retry_log(repo_label):
+    if repo_label not in g.cfg['repo']:
+        abort(404)
+
     logger = g.logger.getChild('retry_log')
 
     lazy_debug(logger, lambda: 'repo_label: {}'.format(repo_label))
@@ -983,6 +990,11 @@ def health():
     return 'OK'
 
 
+@error(404)
+def not_found(error):
+    return g.tpls['404'].render()
+
+
 def redirect_to_canonical_host():
     request_url = urllib.parse.urlparse(request.url)
     redirect_url = request_url
@@ -1023,6 +1035,7 @@ def start(cfg, states, queue_handler, repo_cfgs, repos, logger,
     tpls['queue'] = env.get_template('queue.html')
     tpls['build_res'] = env.get_template('build_res.html')
     tpls['retry_log'] = env.get_template('retry_log.html')
+    tpls['404'] = env.get_template('404.html')
 
     g.cfg = cfg
     g.states = states

homu/html/404.html
@@ -1,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Page not found</title>
+        <style>
+            * { font-family: sans-serif; text-align: center; }
+            h1 { font-size: 20px; }
+            p { font-size: 15px; }
+        </style>
+    </head>
+    <body>
+        <h1>Page not found</h1>
+        <p><a href="/">Go back to the index</a></p>
+    </body>
+</html>
+

commit e0bdf18ebf19847d5aba94617abc9597f96657c8

fix broken combined queues
bors.rust-lang.org mentions

> Homu provides a few simple ways to customize the queue's contents to fit your needs:
>
> * queue/rust+cargo will combine the queues of the rust and cargo repos (for example).

but it appears 963762a1 broke this by early aborting to a 404 without checking this case

homu/server.py
@@ -117,7 +117,10 @@ def result(repo_label, pull):
 
 @get('/queue/<repo_label:path>')
 def queue(repo_label):
-    if repo_label not in g.cfg['repo'] and repo_label != 'all':
+
+    labels = repo_label.split('+')
+    if repo_label != 'all' \
+            and any(label not in g.cfg['repo'] for label in labels):
         abort(404)
 
     logger = g.logger.getChild('queue')
@@ -126,19 +129,20 @@ def queue(repo_label):
 
     single_repo_closed = None
     treeclosed_src = None
+    repo_url = None
+
     if repo_label == 'all':
         labels = g.repos.keys()
         multiple = True
-        repo_url = None
     else:
-        labels = repo_label.split('+')
         multiple = len(labels) > 1
-        if repo_label in g.repos and g.repos[repo_label].treeclosed >= 0:
-            single_repo_closed = g.repos[repo_label].treeclosed
-            treeclosed_src = g.repos[repo_label].treeclosed_src
-        repo_url = 'https://github.com/{}/{}'.format(
-            g.cfg['repo'][repo_label]['owner'],
-            g.cfg['repo'][repo_label]['name'])
+        if not multiple:
+            if repo_label in g.repos and g.repos[repo_label].treeclosed >= 0:
+                single_repo_closed = g.repos[repo_label].treeclosed
+                treeclosed_src = g.repos[repo_label].treeclosed_src
+            repo_url = 'https://github.com/{}/{}'.format(
+                g.cfg['repo'][repo_label]['owner'],
+                g.cfg['repo'][repo_label]['name'])
 
     states = []
     for label in labels: